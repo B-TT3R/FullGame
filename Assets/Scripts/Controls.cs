889a3a8ca7fc6d3d8df22fb0a8951c40e2d674ec
B-TT3R <Tyra.Cassar.H63297@Mcast.edu.mt>
2026-05-09 00:20:41 +0200

    Adding a controls panel and BG music rework

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controls : MonoBehaviour
+
+
+private void update()
+{
+    if (Mouse.current.leftButton.wasPressedThisFrame)
+    {
+        
+    }
+
+}
--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -6,6 +6,7 @@ public class GameManagerScript : MonoBehaviour
 
     public GameObject gameOverUI;
     public GameObject timesuptext;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
